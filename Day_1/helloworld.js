50c29431eb79d263ca381179923433a56ca0ea04
merkaya1 <erkaya.mustafa1@gmail.com>
2023-01-04 13:58:40 +0300

    examples

--- Day_1/helloworld.js
+++ Day_1/helloworld.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+// const str = 'mustafa';
+// let capitalize = str[0].toUpperCase() + str.slice(1)
+
+// console.log(str)
+// console.log('capitalize', capitalize)
+
+const str = 'Bugün hava çok güzel ve yeni bir proje yapmanın zamanı geldi.'
+
+let letter = str.split(' ').map(item => item[0].toUpperCase() + item.substring(1)).join(' ')
+
+console.log('letter', letter)
